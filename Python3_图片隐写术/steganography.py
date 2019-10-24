613a5c968a867e9b3a68a790fe83ba7d54587080
Manchangdx <1195581533@qq.com>
2019-10-24 15:21:11 +0800

    x

--- Python3_图片隐写术/steganography.py
+++ Python3_图片隐写术/steganography.py
@@ -1,4 +1,4 @@
-import sys
+import sys 
 from PIL import Image
 
 
@@ -12,7 +12,7 @@ def make_even_image(image):
     # 这里使用位运算符 >> 和 << 来实现数据处理
     # 奇数减一变偶数，偶数不变，这样处理后，所有数值的最低位变为零
     # pixels 为更改后的像素点数据列表
-    pixels = [(r>>1<<1, g>>1<<1, b>>1<<1, a>>1<<1) 
+    pixels = [(r >> 1 << 1, g >> 1 << 1, b >> 1 << 1, a >> 1 << 1) 
             for r, g, b, a in image.getdata()]  
     # 调用 Image 的 new 方法创建一个相同大小的图片副本
     # 参数为模式（字符串）和规格（二元元组）
@@ -62,17 +62,45 @@ def encode_data_in_image(image, data):
 def binary_to_string(binary):
     '''将二进制字符串转换为 UTF-8 字符串
     '''
+    # 参数 binary 为二进制字符串，且长度为 8 的倍数
+    # index 是 binary 的索引，初始值为 0
     index = 0
-    string = []
-    rec = lambda x, i: x[2:8] + (rec(x[8:], i-1) if i > 1 else '') if x else ''
-    # rec = lambda x, i: x and (x[2:8] + (i > 1 and rec(x[8:], i-1) or '')) or ''
-    fun = lambda x, i: x[i+1:8] + rec(x[8:], i-1)
+    # 创建空列表，以用于添加解析得到的人类能够看懂的 UTF-8 字符
+    strings = []
+
+    # 创建嵌套函数，参数为二进制字符串片段和字节数，返回值为有效二进制字符串
+    def effective_binary(binary_part, zero_index):
+        if not zero_index:
+            return binary_part[1:]
+        binary_list = []
+        for i in range(zero_index):
+            small_part = binary_part[8 * i: 8 * i + 8]
+            binary_list.append(small_part[small_part.find('0') + 1:])
+        return ''.join(binary_list)
+
     while index + 1 < len(binary):
-        chartype = binary[index:].index('0') # 存放字符所占字节数，一个字节的字符会存为 0
-        length = chartype*8 if chartype else 8
-        string.append(chr(int(fun(binary[index:index+length],chartype),2)))
+        # 字符所占字节数举例：
+        # 字符 'a' 转换为字节码后，占 1 个字节，且字节的最高位是 0
+        # 字符 '哈' 转换为字节码后，占 3 个字节，且第一个字节的最高位是 1110
+        # 占 1 个字节，开头是 0xxxxxxx
+        # 占 2 个字节，开头是 110xxxxx
+        # 占 3 个字节，开头是 1110xxxx
+        # 占 4 个字节，开头是 11110xxx
+        # zero_index 为第一个 ‘0’ 的索引，根据它我们可以知道字节数
+        zero_index = binary[index:].index('0') 
+        # 单个字符转换为二进制字节码后的长度：字节数乘以 8
+        length = zero_index * 8 if zero_index else 8
+        # 调用 effective_binary 函数，参数有两个：binary 片段，字节数
+        # 返回的是有效二进制字符串，该字符串通过 int 方法转换成十进制数值
+        # 然后使用 chr 方法即可获得对应的字符啦
+        string = chr(int(effective_binary(
+                binary[index: index+length], zero_index), 2))
+        # 将字符添加到字符列表 strings 中
+        strings.append(string)
+        # 索引变量增加已经处理的二进制字符串长度
         index += length
-    return ''.join(string)
+    # 最后返回解析得到的字符串
+    return ''.join(strings)
 
 
 def decode_data_from_image(image):
